317d8d8c212df2c3a4dbff23d82b837189b54c88
qw572386 <735520121@qq.com>
2018-10-15 22:45:03 +0800

    vuex基础之getters

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -1,5 +1,11 @@
 const state = {
   //
+  userName: 'qiwei'
+}
+const getters = {
+  firstLetter: (state) => {
+    return state.userName.substr(0, 1)
+  }
 }
 const mutations = {
   //
@@ -9,7 +15,9 @@ const actions = {
 }
 
 export default {
+  namespaced: true,
   state,
+  getters,
   mutations,
   actions
 }
--- src/views/store.vue
+++ src/views/store.vue
@@ -1,18 +1,53 @@
 <template>
   <div>
     <a-input @input="handleInput" />
-    <p>{{ inputValue }}</p>
+    <p>{{ inputValue }} last letter is : {{ inputValueLastLetter }}</p>
     <a-show :content="inputValue" />
+    <p>
+      <!-- appName: {{ appName }}
+      <br />
+      appNameWithVersion: {{ appNameWithVersion }} -->
+      <br />
+      userName: {{ userName }}
+      <br />
+      userName first letter is: {{ firstLetter }}
+    </p>
   </div>
 </template>
 <script>
 import AInput from '_c/AInput.vue'
 import AShow from '_c/AShow.vue'
+import { mapState, mapGetters } from 'vuex'
 export default {
   name: 'store',
   data () {
     return {
       inputValue: ''
+    }
+  },
+  computed: {
+   ...mapState({
+     appName: state => state.appName,
+     userName: state => state.user.userName
+   }),
+  // ...mapState([
+  //   appName
+  // ])
+   // appName () {
+    //   return this.$store.state.appName
+    // },
+    // userName () {
+    //   return this.$store.state.user.userName
+    // }
+    // appNameWithVersion () {
+    //   return this.$store.getters.appNameWithVersion
+    // },
+    ...mapGetters('user', [
+      // 'appNameWithVersion',
+      'firstLetter'
+    ]),
+    inputValueLastLetter () {
+      return this.inputValue.substr(-1,1)
     }
   },
   components: {
